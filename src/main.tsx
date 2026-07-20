4a793f4c1edba7ca389e9d5af1153d8ed856c811
Rashisky <rashisky007@gmail.com>
2026-07-20 21:20:49 +0100

    update from Login.css to LoginForm.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-// import App from "./App.tsx";
 import { router } from "./routes/index.tsx";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
@@ -7,7 +6,7 @@ import "./styles/global.css";
 import "./components/layouts/AppLayout.css";
 import "./pages/Posts/Posts.css";
 import "./pages/PostDetail/PostDetail.css";
-import "./pages/Login/Login.css";
+import "./pages/Login/LoginForm.css";
 import "./pages/CreatePosts/CreatePost.css";
 import "./components/Footer/Footer.css";
 import "./pages/Register/Register.css";
